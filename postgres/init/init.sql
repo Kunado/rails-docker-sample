6aa1efc272d0fc3a6f85cf73172316eb5d5dd963
haru911-99 <haruki08070743@gmail.com>
2024-06-17 08:47:17 +0900

    dbをhostにbindしないように修正

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -1,23 +1,3 @@
-BEGIN
-  IF EXISTS (SELECT FROM pg_database WHERE datname = 'development_app') THEN
-    RAISE NOTICE 'development_app already exists';
-  ELSE
-    CREATE DATABASE development_app;
-  END IF;
-END $$
-
-BEGIN
-  IF EXISTS (SELECT FROM pg_database WHERE datname = 'test_app') THEN
-    RAISE NOTICE 'test_app already exists';
-  ELSE
-    CREATE DATABASE test_app;
-  END IF;
-END $$
-
-BEGIN
-  IF EXISTS (SELECT FROM pg_database WHERE datname = 'production_app') THEN
-    RAISE NOTICE 'production_app already exists';
-  ELSE
-    CREATE DATABASE production_app;
-  END IF;
-END $$
+CREATE DATABASE database_name;
+CREATE DATABASE test_app;
+CREATE DATABASE production_app;
